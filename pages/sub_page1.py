4be1309c7d2bd5c02fa22f94128f41b72d961e18
csjryt <csjryt@163.com>
2024-11-06 10:49:19 +0800

    first commit

---
+++ pages/sub_page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Sub Page 1 ❄️")
+st.sidebar.markdown("# Sub Page 1 ❄️")
---
+++ pages/sub_page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Sub Page 2 🎉")
+st.sidebar.markdown("# Sub Page 2 🎉")
